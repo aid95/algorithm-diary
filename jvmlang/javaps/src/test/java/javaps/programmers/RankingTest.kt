9c1e0412ac236e996eb08cd5fdb0d60f2811f604
Byeongju, Shin <cd80@kakao.com>
2021-10-30 23:16:26 +0900

    solved 1 problem

---
+++ jvmlang/javaps/src/test/java/javaps/programmers/RankingTest.kt
@@ -0,0 +1,26 @@
+package javaps.programmers
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class RankingTest {
+
+    @Test
+    fun solution() {
+        // Given
+        val n = 5
+        val results = arrayOf(
+            intArrayOf(4, 3),
+            intArrayOf(4, 2),
+            intArrayOf(3, 2),
+            intArrayOf(1, 2),
+            intArrayOf(2, 5),
+        )
+
+        // When
+        val ans = Ranking().solution(n, results)
+
+        // Then
+        assertEquals(2, ans)
+    }
+}
